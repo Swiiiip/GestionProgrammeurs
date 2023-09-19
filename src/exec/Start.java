a1a63b1573751781b23f739a405373a7df2d99e3
mrredcoding <cedric.alonso.pro@gmail.com>
2023-09-19 09:20:26 +0200

    JavaDoc : Start

--- src/exec/Start.java
+++ src/exec/Start.java
@@ -1,8 +1,23 @@
 package exec;
 
+/**
+ * La classe de démarrage de l'application.
+ * Cette classe contient la méthode principale (main) qui initialise et lance le menu de l'application.
+ *
+ * @author Alonso Cédric
+ * @author Hatoum Jade
+ */
 public class Start {
+
+    /**
+     * Méthode principale (main) de l'application.
+     * Cette méthode initialise et démarre le menu de l'application.
+     *
+     * @param args Les arguments de la ligne de commande (non utilisés dans notre cas).
+     */
     public static void main(String[] args) {
         Menu menu = new Menu();
         menu.start();
     }
 }
+
