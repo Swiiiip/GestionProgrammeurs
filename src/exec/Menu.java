6a8b5ece3f983ed4b2567acf0853d35aa41fffcb
mrredcoding <cedric.alonso.pro@gmail.com>
2023-09-19 09:01:04 +0200

    JavaDoc : Menu

--- src/exec/Menu.java
+++ src/exec/Menu.java
@@ -7,19 +7,57 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Scanner;
 
+
+/**
+ * La classe Menu gère un menu interactif permettant à l'utilisateur d'effectuer
+ * différentes opérations sur une base de données de programmeurs.
+ *
+ * L'utilisateur peut afficher tous les programmeurs, afficher un programmeur en
+ * particulier, supprimer un programmeur, ajouter un programmeur, modifier le salaire
+ * d'un programmeur ou quitter le programme.
+ *
+ * Cette classe étend ActionsBDDImpl, ce qui suggère qu'elle implémente des actions
+ * liées à la base de données.
+ *
+ * @author Alonso Cédric
+ * @author Hatoum Jade
+ */
 public class Menu extends ActionsBDDImpl {
 
+    /**
+     * Scanner utilisé pour lire l'entrée utilisateur.
+     */
     private static final Scanner sc = new Scanner(System.in);
+
+    /**
+     * Choix de l'utilisateur.
+     */
     private int choice;
+
+    /**
+     * Identifiant d'un programmeur.
+     */
     private int id;
+
+    /**
+     * Objet ProgrammeurBean pour stocker les données d'un programmeur.
+     */
     private ProgrammeurBean prog;
 
+    /**
+     * Constructeur par défaut de la classe Menu. Initialise les variables de choix,
+     * d'identifiant et de programmeur.
+     */
     public Menu(){
         this.choice = 0;
         this.id = 0;
         this.prog = null;
     }
 
+    /**
+     * Démarre l'application et gère le menu interactif. L'utilisateur peut choisir
+     * parmi différentes options pour interagir avec la base de données des programmeurs.
+     */
     public void start() {
 
         boolean on = true;
@@ -122,6 +160,16 @@ public class Menu extends ActionsBDDImpl {
         sc.close();
     }
 
+    /**
+     * Affiche un menu interactif à l'utilisateur, présentant les différentes options
+     * disponibles pour interagir avec la base de données des programmeurs. Les options
+     * incluent l'affichage de tous les programmeurs, l'affichage d'un programmeur
+     * spécifique, la suppression d'un programmeur, l'ajout d'un nouveau programmeur,
+     * la modification du salaire d'un programmeur, et la sortie du programme.
+     *
+     * Le menu affiché contient une liste numérotée d'options permettant à l'utilisateur
+     * de sélectionner l'action souhaitée en saisissant le numéro correspondant.
+     */
     public void displayMenu() {
         System.out.println("\n<<<<<<<<<<<<  MENU  >>>>>>>>>>>>\n");
         System.out.println("1. Afficher tous les programmeurs");
@@ -133,31 +181,67 @@ public class Menu extends ActionsBDDImpl {
         System.out.print("Quel est votre choix ? : ");
     }
 
+    /**
+     * Obtient un entier correspondant au choix d'action à partir de l'entrée utilisateur.
+     *
+     * @return L'entier saisi par l'utilisateur.
+     */
     public int getChoice() {
-        Scanner sc = new Scanner(System.in);
-
         return sc.nextInt();
     }
 
-    public float getSalary(){
+    /**
+     * Obtient un montant de salaire à partir de l'entrée utilisateur.
+     *
+     * @return Le montant du salaire saisi par l'utilisateur.
+     */
+    public float getSalary() {
         return sc.nextFloat();
     }
 
+    /**
+     * Affiche un message d'erreur à l'utilisateur.
+     *
+     * @param message Le message d'erreur à afficher.
+     */
     public void displayError(String message){
         System.err.println("\n" + message+"\n");
     }
 
+    /**
+     * Affiche les informations de tous les programmeurs contenus dans une liste.
+     * Pour afficher les détails de chaque programmeur individuellement, cette méthode
+     * utilise la méthode {@link #displayProg(ProgrammeurBean)}.
+     *
+     * @param progs Liste des programmeurs à afficher.
+     */
     public void displayAllProgs(List<ProgrammeurBean> progs){
         for(ProgrammeurBean prog : progs){
             displayProg(prog);
         }
     }
 
+    /**
+     * Affiche les informations d'un programmeur, y compris son identifiant en base de données
+     * son nom, prénom, adresse, pseudo, responsable, hobby, année de naissance, salaire et prime.
+     *
+     * @param prog ProgrammeurBean contenant les informations à afficher.
+     */
     public void displayProg(ProgrammeurBean prog){
         System.out.println(prog);
         System.out.println("---------------------------------------------------------------------------------------\n");
     }
 
+    /**
+     * Obtient les informations d'un nouveau programmeur à partir de l'entrée
+     * utilisateur en demandant diverses données telles que le nom, le prénom,
+     * l'adresse, le pseudo, le responsable, le passe-temps, l'année de naissance,
+     * le salaire et la prime. Ces informations sont utilisées pour créer un
+     * nouvel objet ProgrammeurBean représentant le programmeur.
+     *
+     * @return Un nouvel objet ProgrammeurBean initialisé avec les informations
+     *         saisies par l'utilisateur pour le nouveau programmeur.
+     */
     public ProgrammeurBean getProg(){
         System.out.print("Nom du programmeur : ");
         String firstName = sc.next();
@@ -187,8 +271,6 @@ public class Menu extends ActionsBDDImpl {
         float prime = sc.nextFloat();
 
         return new ProgrammeurBean(firstName, lastName, address, pseudo, manager, hobby, birthYear, salary, prime);
-
-
     }
 
 }
